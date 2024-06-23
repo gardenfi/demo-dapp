96740f089ed54d7da70ef50a02d649712b824b17
Sushants-Git <sushantsgml@gmail.com>
2024-06-23 18:29:31 +0530

    fix: Top level await

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), nodePolyfills()],
+  plugins: [react(), wasm(), nodePolyfills(),topLevelAwait()],
 });
